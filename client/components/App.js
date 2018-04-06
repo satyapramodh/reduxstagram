9b09c2a5671a0216fe4248a58bce8b46165d9fdc
Pramodh Mazumdar <satyapramodh@gmail.com>
2018-04-05 21:07:51 -0700

    add bind creators, photo and comments components

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,13 @@
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import * as actionCreators from "../actions/actionCreators";
-import Main from "./Main";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
 
 function mapStateToProps(state) {
   return {
     posts: state.posts,
     comments: state.comments
-  };
+  }
 }
 
 function mapDispachToProps(dispatch) {
